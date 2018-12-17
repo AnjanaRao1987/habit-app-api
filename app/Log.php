fc62e4a7e5632175fcdfaa37532a08edaa2683a5
Jack Standbridge <jack.s.standbridge@gmail.com>
2018-12-17 16:59:31 +0000

    complete Log model

--- app/Log.php
+++ app/Log.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log extends Model
 {
-    //
+    protected $fillable = ['habit_id', 'date'];
+
+    public function habit()
+    {
+    	return $this->belongsTo(Habit::class);
+    }
 }
